5553cbd2b5f0dad8fede5c5f0508123a81028d5e
Dan Rabinowitz <djr@DanielRabinowitz.com>
2015-03-24 22:38:02 -0600

    Use actual DNS servers from resolv.conf

--- iptables_d/files/etc/iptables.d/50_allow_basic_outgoing.sh
+++ iptables_d/files/etc/iptables.d/50_allow_basic_outgoing.sh
@@ -1,6 +1,7 @@
 # DNS
-DNS_SERVER="8.8.8.8 10.0.2.3"
-for ip in $DNS_SERVER
+# DNS_SERVER="8.8.8.8 10.0.2.3"
+DNS_SERVERS=$(cat /etc/resolv.conf |sed -rn 's/^nameserver ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})$/\1/p')
+for ip in $DNS_SERVERS
 do
 $IPT -A OUTPUT -p udp --sport 1024:65535 -d $ip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
 $IPT -A INPUT -p udp -s $ip --sport 53 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
