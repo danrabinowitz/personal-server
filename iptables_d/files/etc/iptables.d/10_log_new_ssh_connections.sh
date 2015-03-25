c042ecf0892bc8131c88581a2f62bb4d017729eb
Dan Rabinowitz <djr@DanielRabinowitz.com>
2015-03-25 11:13:16 -0600

    Stop logging ssh connections

--- iptables_d/files/etc/iptables.d/10_log_new_ssh_connections.sh
+++ iptables_d/files/etc/iptables.d/10_log_new_ssh_connections.sh
@@ -1,2 +1,2 @@
 # Log new ssh connections
-$IPT -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW -j LOG --log-prefix "${IPTABLES_LOG_PREFIX}: ssh connection: "
+# $IPT -A INPUT -p tcp --dport 22 -m state --state NEW -j LOG --log-prefix "${IPTABLES_LOG_PREFIX}: ssh connection: "
