a94f430e090ef9811b16f7d3d286865b0b0d2b88
Dan Rabinowitz <djr@DanielRabinowitz.com>
2015-03-30 17:03:38 -0600

    Add more allowed ssh ips

--- custom_iptables_rules_1/files/etc/iptables.d/50_allow_outgoing_ssh.sh
+++ custom_iptables_rules_1/files/etc/iptables.d/50_allow_outgoing_ssh.sh
@@ -1,5 +1,9 @@
-REMOTE_SERVERS=('192.241.215.95')
-for ip in $REMOTE_SERVERS; do
+# insightseminars-linux-01="192.241.215.95"
+# usm-portal-staging="54.152.188.42"
+# usm-portal="54.88.102.204"
+remote_servers=("192.241.215.95" "54.152.188.42" "54.88.102.204")
+for ip in "${remote_servers[@]}"; do
 $IPT -A INPUT  -i $PUBLIC_INT -p tcp -s $ip --sport 22 -m state --state ESTABLISHED -j ACCEPT
 $IPT -A OUTPUT -o $PUBLIC_INT -p tcp -d $ip --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+true
 done
