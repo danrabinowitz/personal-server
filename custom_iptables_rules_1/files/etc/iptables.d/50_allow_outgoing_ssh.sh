167000cf0fb13e31ea41ecfa01e12da729dcdd0c
Dan Rabinowitz <djr@DanielRabinowitz.com>
2015-03-31 21:17:20 -0600

    Add outgoing ip for ssh

--- custom_iptables_rules_1/files/etc/iptables.d/50_allow_outgoing_ssh.sh
+++ custom_iptables_rules_1/files/etc/iptables.d/50_allow_outgoing_ssh.sh
@@ -1,7 +1,8 @@
 # insightseminars-linux-01="192.241.215.95"
 # usm-portal-staging="54.152.188.42"
 # usm-portal="54.88.102.204"
-remote_servers=("192.241.215.95" "54.152.188.42" "54.88.102.204")
+# usm-awsutil1="54.210.85.144"
+remote_servers=("192.241.215.95" "54.152.188.42" "54.88.102.204" "54.210.85.144")
 for ip in "${remote_servers[@]}"; do
 $IPT -A INPUT  -i $PUBLIC_INT -p tcp -s $ip --sport 22 -m state --state ESTABLISHED -j ACCEPT
 $IPT -A OUTPUT -o $PUBLIC_INT -p tcp -d $ip --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
